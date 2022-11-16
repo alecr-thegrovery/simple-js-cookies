f74fd19ecd733fa5d2f86908c9403b3d40d33189
Alec Reimel <alecreimel1@gmail.com>
2022-11-16 11:47:18 -0500

    code finalized, readme updated, test page styled

--- README.md
+++ README.md
@@ -3,9 +3,26 @@
 ---
 
 ## Usage
-Use this code to look for a localStorage variable and perform an action based on it.
+Use this code to look for a localStorage variable and perform an action based on it.  Just add the index.js script to the page and wrap your gated content in the HTML code below.  
+
+```
+	<div class="gatedContent" data-gated-content="">
+		GATED CONTENT SUCCESS
+	</div>
+
+	<div class="gatedContentWarning" data-gated-content="">
+		GATED CONTENT WARNING
+	</div>
+```
+
+## Requirements
+Requires jQuery.
+
+## Notes
+- localStorage: long term storage for the browser.
+- sessionStorage: short term storage just for the current tab.
 
 ## Coming Soon
 - Publish as an NPM package
 - Add localStorage/sessionStorage option
-- 
+- expand options to use in different use cases.
--- index.js
+++ index.js
@@ -1,23 +1,57 @@
 $(document).ready(function(){
+	//console.log("script start");
 	var targetParam = "gated_content";
-	var targetParamUnlock = true;
+	var targetParamUnlock = "unlocked";
 	var gatedContentElement = $("[data-gated-content]");
+	gatedContentCSS();
+	gatedContent(targetParam, targetParamUnlock);
 
-	function gatedContent(targetParam, targetParamVal) {
+	function gatedContent(targetParam, targetParamUnlock) {
+		console.log("gatedContent start");
 		var searchParams = new URLSearchParams(window.location.search); 
 
-		if(searchParams.has(targetParam)) {
+		if(searchParams.get(targetParam)===targetParamUnlock) {
 			//Parameter is present in URL.  Grab and store parameter value
 			//Add parameter to localStorage, then show gated content
+			//console.log("gatedContent if 1");
 			var targetParamVal = searchParams.get(targetParam);
 			localStorage.setItem(targetParam, targetParamVal);
+			
+			gatedContentShow();
 		}else if(localStorage.getItem(targetParam)){
 			//Parameter is not present in URL, but is present in localStorage
 			//show gated content
+			//console.log("gatedContent if 2");
+			gatedContentShow();
 		} else{
 			//Parameter is not present in URL or localStorage
 			//Do nothing, or display gated content warning
+			//console.log("gatedContent else");
 		}
+	};
+
+	function gatedContentShow(){
+		$("[data-gated-content]").attr("data-gated-content", "unlocked");
+	};
+
+	function gatedContentCSS(){
+		var css = '\
+			<style type="text/css">\
+				.gatedContent{\
+					display: none;\
+				}\
+				.gatedContentWarning{\
+					display: block;\
+				}\
+				.gatedContent[data-gated-content="unlocked"]{\
+					display: block;\
+				}\
+				.gatedContentWarning[data-gated-content="unlocked"]{\
+					display: none;\
+				}\
+			</style>\
+		';
+		$("body").append(css);
 	};
 
 });
